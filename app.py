910a863ffb06b89c883c7d922a823dce410ab923
Mahesh Vaidya <forvaidya@gmail.com>
2021-12-09 17:52:12 +0530

    docstrng2
    docstring2

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+"""
+Customery Hello World app.
+"""
+
 app = Flask(__name__)
 
-"""
-
-Customery Hello World app.
-
-"""
 @app.route("/")
 def hello():
+    """
+        Execution::Customery Hello World app.
+    """
     return "Hello, World!"
+
